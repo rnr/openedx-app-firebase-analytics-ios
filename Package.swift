1bcc9513b3e3d67b024157ca5e02ba5ab68a3041
IvanStepanok <ivan.stepanok@raccoongang.com>
2024-10-14 13:27:27 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OEXFirebaseAnalytics",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "OEXFirebaseAnalytics",
+            targets: ["OEXFirebaseAnalytics"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/raccoongang/openedx-app-foundation-ios.git", branch: "73836f22f165d114e07e309a01a5cbcd8103dbec"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.3.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "OEXFirebaseAnalytics",
+            dependencies: [
+                .product(name: "OEXFoundation", package: "openedx-app-foundation-ios"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk")
+            ]
+        ),
+        .testTarget(
+            name: "OEXFirebaseAnalyticsTests",
+            dependencies: ["OEXFirebaseAnalytics"]
+        ),
+    ]
+)
---
+++ Tests/OEXFirebaseAnalyticsTests/OEXFirebaseAnalyticsTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import OEXFirebaseAnalytics
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
